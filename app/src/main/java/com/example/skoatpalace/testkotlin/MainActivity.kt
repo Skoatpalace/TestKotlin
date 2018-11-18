c685abcc1c3aeb5fe3110701e6c6fde9df45b5d5
Skoatpalace <richard.fages@gmail.com>
2018-11-18 09:24:42 +0100

    FizzBuzz

--- app/src/main/java/com/example/skoatpalace/testkotlin/MainActivity.kt
+++ app/src/main/java/com/example/skoatpalace/testkotlin/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.skoatpalace.testkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.setContentView(R.layout.activity_main)
+
+        val a: Int = 2
+        val b: Int = 3
+        button_test.setOnClickListener {
+            Toast.makeText(this@MainActivity, a + b, Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+
+    fun FuzzBizz(args: Array<String>) = (1..100).map {
+        StringBuilder().apply {
+            if (it % 3 == 0) append("Fizz")
+            if (it % 5 == 0) append("Buzz")
+            if (isEmpty()) append(it)
+        }.toString()
+
+    }.forEach {
+
+        // V
+        println(it)
+
     }
 }
